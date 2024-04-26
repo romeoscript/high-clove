80cde7d028aee50cf4a18d45ba3163bd65fa2bfd
Romanus Chukwuemeka <romeobourne211@gmail.com>
2024-04-26 16:25:26 +0100

    Update customlayout.blade.php

--- resources/views/layouts/customlayout.blade.php
+++ resources/views/layouts/customlayout.blade.php
@@ -512,8 +512,18 @@ video {
 
 
 
-<script src="//code.tidio.co/3wcdivpmwspggjxltzpalc7ijwsaroso.js" async></script>
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '596930ab156b390313734ff5a08d7f5390c75813';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
 <style type="text/css">
     li.export-main {
